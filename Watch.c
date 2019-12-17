d2885b821b7945866ce80ec67136cd6658c66247
Syncreator <51112090+syncreator@users.noreply.github.com>
2019-12-17 13:52:39 +0200

    Update Watch.c

--- Watch.c
+++ Watch.c
@@ -1,6 +1,22 @@
 #includ
-
-void Watch()
-{ unsigned long timestart; // Переменная для хранения точки отсчета
-  timestart=millis();
+//Возвращает время в минутах на текущий момент вызова (диапазон 24часа)
+int Watch(int Start_Time=0, unsigned long X_Point=0,)
+{ int static s(0);
+  unsigned long static cur_time=millis();
+  unsigned long Temp_time; //переменная для промежуточных вычислений 
+    Temp_time=Start_Time+(0<=(cur_time-X_Point));
+    if(cur_time+1000<=millis())
+    { cur_time=millis();s++;
+     if(!_Menu)
+      { myOLED.drawRoundRect(90, 15, 127, 26);//?стирачка??????
+        myOLED.update();
+        myOLED.printNumI(Temp-time/60,40, 57);//Вывод в граф меню часы
+        myOLED.printNumI(Temp-time%60,50, 57);//Вывод в граф меню минуты
+       if(s%2)myOLED.print(":", 106,57);else myOLED.print(" ", 106,57);//мигалка точечками
+        myOLED.update();
+      }
+    }
+  //cur_time=millis();
+     if()
+ 
 }
