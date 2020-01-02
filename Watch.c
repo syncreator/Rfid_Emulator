f7ab812dacb1218966deaba80d09b5c4c74581a5
Syncreator <51112090+syncreator@users.noreply.github.com>
2020-01-02 19:14:00 +0200

    Update Watch.c

--- Watch.c
+++ Watch.c
@@ -6,8 +6,8 @@ int Watch(unsigned int St_time=0, unsigned long X_point=0,)//St_time точка 
   unsigned long Temp_time; //переменная для промежуточных вычислений 
     //Temp_time=St_time+(((cur_time-X_Point)/1000/60)%1440);//текущее время в минутах
   
- if((millis()>= X_point)Temp_time=(((millis()-X_Point)/1000/60)%1440>=1440-Start_Time)?Start_Time+((millis()-X_Point)/1000/60)%1440:(((millis()-X_Point)/1000/60)%1440)-(1440-Start_Time);
-    else )Temp_time=(((millis()-X_Point)/1000/60)%1440>=1440-Start_Time)?Start_Time+((millis()-X_Point)/1000/60)%1440:(((millis()-X_Point)/1000/60)%1440)-(1440-Start_Time);
+ if((millis()>= X_point)Temp_time=(((millis()-X_Point)/1000)%86400>=(1440-Start_Time)*60)?((millis()-X_Point)/1000)%86400-(1440-Start_Time)*60:Start_Time*60+((millis()-X_Point)/1000)%86400;
+    else Temp_time=(((X_Point-millis())/1000)%86400>=Start_Time*60)?Start_Time*60+(86400-((X_Point-millis())/1000)%86400): Start_Time*60-(X_Point-millis())/1000%86400;
  //Temp_time=(St_time>=(X_point-millis())%1440)? St_time-(X_point-millis())%1440 : St_time+(1440-(X_point-millis())%1440);
  
     if(cur_time+1000<=millis())
