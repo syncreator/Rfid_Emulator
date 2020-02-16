b62ff8ee46a0617bda349dda5d7259c083c46a98
Syncreator <51112090+syncreator@users.noreply.github.com>
2020-02-16 20:47:35 +0200

    Update m_box.c

--- m_box.c
+++ m_box.c
@@ -50,20 +50,71 @@ static const MUSIC ST_WARS[]PROGMEM = {
 };
 //Nokia_tune
 static const MUSIC NOK_TUNE[]PROGMEM = {
-  NOTE_E5, 8, //0
-  NOTE_D5, 8, //1
-  NOTE_F4, 4, //2
-  NOTE_G4, 4, //3
-  NOTE_C5, 8, //4
-  NOTE_B4, 8, //5
-  NOTE_D4, 4, //6
-  NOTE_E4, 4, //7
-  NOTE_B4, 8, //8
-  NOTE_A4, 8, //9
-  NOTE_C4, 4, //10
-  NOTE_E4, 4, //11
-  NOTE_A4, 1, //12
-}
+  NOTE_E5, 1000/8, //0
+  NOTE_D5, 1000/8, //1
+  NOTE_F4, 1000/4, //2
+  NOTE_G4, 1000/4, //3
+  NOTE_C5, 1000/8, //4
+  NOTE_B4, 1000/8, //5
+  NOTE_D4, 1000/4, //6
+  NOTE_E4, 1000/4, //7
+  NOTE_B4, 1000/8, //8
+  NOTE_A4, 1000/8, //9
+  NOTE_C4, 1000/4, //10
+  NOTE_E4, 1000/4, //11
+  NOTE_A4, 1000/1, //12
+};
+//Pacman_#1
+static const MUSIC PACMAN_1[]PROGMEM = {
+  NOTE_B5, 1000/6, //0
+  NOTE_B6, 1000/6, //1
+  NOTE_FS6, 1000/6, //2
+  NOTE_DS6, 1000/6, //3
+  NOTE_B6, 1000/6, //4
+  NOTE_F6, 1000/6, //5
+  NOTE_E6, 1000/6, //6
+  NOTE_C5, 1000/6, //7
+  NOTE_C6, 1000/6, //8
+  NOTE_G6, 1000/6, //9
+  NOTE_F5, 1000/6, //10
+  NOTE_C6, 1000/6, //11
+  NOTE_G6, 1000/6, //12 
+  NOTE_E6, 1000/6, //13
+};
+//Pacman_#2
+static const MUSIC PACMAN_2[]PROGMEM = {
+  NOTE_B4, 1000/16, //0
+  NOTE_B5, 1000/16, //1
+  NOTE_FS5, 1000/16, //2 
+  NOTE_DS5, 1000/16, //3
+  NOTE_B5, 1000/32, //4
+  NOTE_FS5, 1000/16, //5
+  NOTE_DS5, 1000/8, //6
+  NOTE_C5, 1000/16, //7
+  NOTE_C6, 1000/16, //8
+  NOTE_G6, 1000/16, //9
+  NOTE_E6, 1000/16, //10 
+  NOTE_C6, 1000/32, //11
+  NOTE_G6, 1000/16, //12 
+  NOTE_E6, 1000/8, //13
+  NOTE_B4, 1000/16, //14
+  NOTE_B5, 1000/16, //15
+  NOTE_FS5, 1000/16, //16
+  NOTE_DS5, 1000/16, //17
+  NOTE_B5, 1000/32, //18
+  NOTE_FS5, 1000/16, //19
+  NOTE_DS5, 1000/8, //20
+  NOTE_DS5, 1000/32, //21
+  NOTE_E5, 1000/32, //22
+  NOTE_F5, 1000/32, //23
+  NOTE_F5, 1000/32, //24
+  NOTE_FS5, 1000/32, //25
+  NOTE_G5, 1000/32, //26
+  NOTE_G5, 1000/32, //27
+  NOTE_GS5, 1000/32, //28
+  NOTE_A5, 1000/16, //29
+  NOTE_B5, 1000/8, //30
+};
 
 void Music(byte n, byte Pin_tone))//номер мелодии или указатель на мелодию?
 {/// звуки для оформления
