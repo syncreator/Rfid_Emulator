c2e5f16e1d7e2594483e6ac4cb5b9fbc5a05c9d8
Syncreator <51112090+syncreator@users.noreply.github.com>
2020-03-10 08:14:19 +0200

    Update m_box.c

--- m_box.c
+++ m_box.c
@@ -3,6 +3,65 @@
 #include <OLED_I2C.h>
 #include <Button.h>
 
+#define NOTE_C4 262
+#define NOTE_CS4 277
+#define NOTE_D4 294
+#define NOTE_DS4 311
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_FS4 370
+#define NOTE_G4 392
+#define NOTE_GS4 415
+#define NOTE_A4 440
+#define NOTE_AS4 466
+#define NOTE_B4 494
+#define NOTE_C5 523
+#define NOTE_CS5 554
+#define NOTE_D5 587
+#define NOTE_DS5 622
+#define NOTE_E5 659
+#define NOTE_F5 698
+#define NOTE_FS5 740
+#define NOTE_G5 784
+#define NOTE_GS5 831
+#define NOTE_A5 880
+#define NOTE_AS5 932
+#define NOTE_B5 988
+#define NOTE_C6 1047
+#define NOTE_CS6 1109
+#define NOTE_D6 1175
+#define NOTE_DS6 1245
+#define NOTE_E6 1319
+#define NOTE_F6 1397
+#define NOTE_FS6 1480
+#define NOTE_G6 1568
+#define NOTE_GS6 1661
+#define NOTE_A6 1760
+#define NOTE_AS6 1865
+#define NOTE_B6 1976
+#define NOTE_C7 2093
+#define NOTE_CS7 2217
+#define NOTE_D7 2349
+#define NOTE_DS7 2489
+#define NOTE_E7 2637
+#define NOTE_F7 2794
+#define NOTE_FS7 2960
+#define NOTE_G7 3136
+#define NOTE_GS7 3322
+#define NOTE_A7 3520
+#define NOTE_AS7 3729
+#define NOTE_B7 3951
+#define NOTE_C8 4186
+#define NOTE_CS8 4435
+#define NOTE_D8 4699
+#define NOTE_DS8 4978
+
+extern uint8_t SmallFont[];
+extern uint8_t BigNumbers[];
+extern uint8_t MediumNumbers[];
+extern uint8_t arrow_13x10[];
+extern uint8_t check_14x10[];
+
 //Структура мелодии
 struct MUSIC{
   int note;
@@ -170,10 +229,10 @@ static const MUSIC C_GENA[]PROGMEM = {
   0,0,
 };
 
-void Music(byte m, byte Pin_tone, char* melody_name))//номер мелодии, указатель на название мелодии
+void Music(OLED &myOLED, Button &button1, Button &button2,byte m, byte Pin_tone, char* melody_name)//номер мелодии, указатель на название мелодии
 { MUSIC *bm ;
    bool ts(0);//переменная для включения/выключения паузы в плеере
-   int n(0),m(0);//для расчета кол-ва нот в массиве
+   int n(0),k(0);//для расчета кол-ва нот в массиве
    float s;//расчет статуса проигрыша мелодии
    unsigned long x;
   if(m==5)
@@ -346,15 +405,15 @@ void Music(byte m, byte Pin_tone, char* melody_name))//номер мелодии
     }
    noTone(Pin_tone);
 }//if end*/
-    if(ts && m<(n-2))//если в режиме PLAY и сыграны не все ноты
+    if(ts && k<(n-2))//если в режиме PLAY и сыграны не все ноты
  //while(bm->note)
      {
-      m++;
-      myOLED.drawRoundRect(9, 40, 9+round(m*s), 53);
-      myOLED.printNumI(round(m*s),15,56,3,'0');
+      k++;
+      myOLED.drawRoundRect(9, 40, 9+round(k*s), 53);
+      myOLED.printNumI(round(k*s),15,56,3,'0');
       myOLED.print("%", 40, 56);
       myOLED.update();
-    tone(Pin_tone, bx->note, bm->dur);
+    tone(Pin_tone, bm->note, bm->dur);
     //digitalWrite(13, HIGH);
     //delay(bm->dur);
       //начало блока вызова цикла задержки аналог delay()
