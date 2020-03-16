30c92815e14c53f9e96707ed53a6baa1e4fbf840
Syncreator <51112090+syncreator@users.noreply.github.com>
2020-03-16 12:36:52 +0200

    Update Torch.c

--- Torch.c
+++ Torch.c
@@ -4,6 +4,9 @@ int led_pin_green1 = 5; // пин подключения светика green1
 int led_pin_green2 = 5; // пин подключения светика green2
 int led_pin_blue1 = 5; // пин подключения светика blue1
 int led_pin_blue2 = 5; // пин подключения светика blue2
+////
+boolean status = true; // флаг, что активна левая
+boolean open = true; // флаг, включения
 /////////////
 void setup()
     {
@@ -16,7 +19,7 @@ pinMode( led_pin_blue2,  OUTPUT);
     }
 //////
 ........
- byte del(0);
+ byte del(0);//период мерцания
   while(1)
   {if ( button2.flagClick == true ){button2.flagClick = false;
           if(del>10)del=0;else del++;}
@@ -51,3 +54,15 @@ pinMode( led_pin_blue2,  OUTPUT);
      digitalWrite( led_pin_blue2, LOW);
         break;}
   }
+////
+////-Полицейская мигалка поочереди двумя диодами
+for (int x=0; x<6; x++) { // цикл 6 проходов
+    if (open) // если true
+        digitalWrite(status ? led_3 : led_5, HIGH); // если status == false то падаем на пин 3 иначе на пин 5
+    else // иначе
+      digitalWrite(status ? led_3 : led_5, LOW); // если status == false то падаем на пин 3 иначе на пин 5
+    delay(interval[x]); // Ждем 
+     if (x == 5) // если последний проход по циклу
+        status = !status; // передаем слово (меняем текущий пин) меняем false на true и наоборот;
+    open = !open; //меняем false на true и наоборот;
+   }                      
