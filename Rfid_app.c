dcdcd8108c08d93766ce30896a42a9e44c0c7ba9
Syncreator <51112090+syncreator@users.noreply.github.com>
2020-03-25 13:02:41 +0200

    Update Rfid_app.c

--- Rfid_app.c
+++ Rfid_app.c
@@ -1,1 +1,23 @@
+#include <SoftwareSerial.h>
+SoftwareSerial RfidReader(0, 1);
+int num;
+int count = 0;
+void setup(){
+Serial.begin(9600); 
+RfidReader.begin(9600);
+}
+void loop() 
+{
+ if (RfidReader.available() > 0)
+  {count++;
+num = RfidReader.read();
+Serial.print(num, DEC);
+    if(count >= 14)
+    {Serial.println(' ');
+     count = 0;}
+  else {
+Serial.print(", ");
+  }
+ }
+}
 
